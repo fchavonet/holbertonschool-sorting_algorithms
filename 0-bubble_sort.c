62bb72c99a8c808ea2f00e1458f58d4f5d5145d0
fchavonet <fchavonet@mailo.com>
2023-09-18 19:52:03 +0200

    Task 0 - Bubble sort function.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm.
+ *
+ * @array: the array to be printed.
+ * @size: number of elements in @array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j = 0;
+
+	size_t swap_value = 0;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_value = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = swap_value;
+
+				print_array(array, size);
+			}
+		}
+	}
+}
